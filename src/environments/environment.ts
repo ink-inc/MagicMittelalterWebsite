e5a125903845216a5e5c1e3d5eabd38474e82a85
Friedrich <weitfriedrich@gmail.com>
2020-04-04 21:03:48 +0200

    ADD: firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBT4Z7GNqnVjkWAtS7y5Cb4ujxB6QhXzF4",
+    authDomain: "magicmittelalterwebsite.firebaseapp.com",
+    databaseURL: "https://magicmittelalterwebsite.firebaseio.com",
+    projectId: "magicmittelalterwebsite",
+    storageBucket: "magicmittelalterwebsite.appspot.com",
+    messagingSenderId: "293014852213",
+    appId: "1:293014852213:web:77c65e83c677b2db38de3c",
+    measurementId: "G-W97N9EQDH7"
+  }
 };
 
 /*
