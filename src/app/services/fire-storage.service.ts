081312d55d81d336f6d3df80bb26c28b099af592
Pascal Stockert <pascal.stockert@live.com>
2020-04-05 21:50:20 +0200

    TEST

---
+++ src/app/services/fire-storage.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FireStorageService {
+
+  url: any;
+
+  constructor(private storage: AngularFireStorage) {
+
+  }
+
+  getDownloadURL() {
+    const ref = this.storage.ref('rangaWot.png');
+    return new Promise(function(resolve) {
+      ref.getDownloadURL().subscribe(value => {
+        resolve(value);
+      });
+    });
+  }
+}
