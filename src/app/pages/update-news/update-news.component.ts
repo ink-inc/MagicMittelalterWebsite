559263763a07bfde58dfe93443d5b37d9f9eed78
Friedrich <weitfriedrich@gmail.com>
2020-04-13 17:38:23 +0200

    UPDATE: Update-Wrapper

--- src/app/pages/update-news/update-news.component.ts
+++ src/app/pages/update-news/update-news.component.ts
@@ -7,9 +7,7 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 })
 export class UpdateNewsComponent implements OnInit, AfterViewInit {
 
-  updateOpen = false;
-  readMoreText = "Mehr lesen";
-  readMoreStyle;
+  openUpdates: HTMLCollection = document.getElementsByClassName("update-open");
   headerData = {
     title: "Update-News",
     subtitle: "Moin Leude, hier gibts bald <br> <u>WÖCHENTLICHE UPDATES</u>",
@@ -17,7 +15,7 @@ export class UpdateNewsComponent implements OnInit, AfterViewInit {
   };
 
   constructor() { }
-
+  
   ngOnInit(): void {
   }
 
@@ -25,14 +23,23 @@ export class UpdateNewsComponent implements OnInit, AfterViewInit {
     document.getElementsByClassName("update-wrapper")
   }
 
-  readMore() {
-    this.updateOpen = !this.updateOpen;
-    if (this.updateOpen) {
-      this.readMoreText = "Weniger lesen";
-      this.readMoreStyle = "color: green;";
+  readMore(clickedUpdate) {
+    if (clickedUpdate.classList.length == 2) { //open Update-Wrapper
+      if (this.openUpdates.length > 0) { //check for opened Update-Wrappers
+        for (let i = 0; i < this.openUpdates.length; i++) {
+          this.closeUpdate(this.openUpdates[i]); //close other Update-Wrappers
+        }
+      }
+      clickedUpdate.style.setProperty('Height', clickedUpdate.children[1].offsetHeight + 'px'); //Update-Wrapper Height --> like right-side
+      clickedUpdate.lastChild.innerText = "Weniger lesen";
+      clickedUpdate.classList.add("update-open"); //add class update-open
     } else {
-      this.readMoreText = "Mehr lesen";
+      this.closeUpdate(clickedUpdate); //close Update-Wrapper
     }
   }
-
+  closeUpdate(closingUpdate) { //close Update-Wrapper
+    closingUpdate.style.setProperty('Height', '300px'); //Update-Wrapper Height --> 300px
+    closingUpdate.lastChild.innerText = "Mehr lesen";
+    closingUpdate.classList.remove("update-open"); //remove class update-open
+  }
 }
